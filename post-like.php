1060a0050c4e8648393197474d7870d8e3880670
Jon Masterson <hello@jonmasterson.com>
2013-09-02 20:14:24 -0400

    Update post-like.php

--- post-like.php
+++ post-like.php
@@ -1,10 +1,23 @@
 <?php
-/**
- * Add a local "Like" system to your theme
- * Author: Jon Masterson
- * 
- *
- */
+/*
+Name:  WordPress Post Like System
+Description:  A simple and efficient post like system for WordPress.
+Version:      0.1
+Author:       Jon Masterson
+Author URI:   http://jonmasterson.com/
+License:      GPL v2 or later
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
  
 /**
  * (1) Enqueue scripts for like system
