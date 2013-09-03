896fb78b35f0366512083736d2c5eacc8dd94a28
Jon Masterson <hello@jonmasterson.com>
2013-09-03 16:42:59 -0400

    Removed the plugin bits

--- post-like.php
+++ post-like.php
@@ -5,18 +5,6 @@ Description:  A simple and efficient post like system for WordPress.
 Version:      0.1
 Author:       Jon Masterson
 Author URI:   http://jonmasterson.com/
-License:      GPL v2 or later
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
 */
  
 /**
