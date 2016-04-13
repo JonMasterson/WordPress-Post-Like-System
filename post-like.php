833418f021bd67b5e71a51d919978859d3623f9b
Jon Masterson <hello@jonmasterson.com>
2016-04-13 08:13:42 -0400

    Changed order of ajax url

--- post-like.php
+++ post-like.php
@@ -225,7 +225,7 @@ function get_simple_likes_button( $post_id, $is_comment = NULL ) {
 		$title = __( 'Like', 'YourThemeTextDomain' );
 		$icon = $icon_empty;
 	}
-	$output = '<span class="sl-wrapper"><a href="' . admin_url( 'admin-ajax.php?action=process_simple_like' . '&nonce=' . $nonce . '&post_id=' . $post_id . '&disabled=true&is_comment=' . $is_comment ) . '" class="sl-button' . $post_id_class . $class . $comment_class . '" data-nonce="' . $nonce . '" data-post-id="' . $post_id . '" data-iscomment="' . $is_comment . '" title="' . $title . '">' . $icon . $count . '</a>' . $loader . '</span>';
+	$output = '<span class="sl-wrapper"><a href="' . admin_url( 'admin-ajax.php?action=process_simple_like' . '&post_id=' . $post_id . '&nonce=' . $nonce . '&is_comment=' . $is_comment . '&disabled=true' ) . '" class="sl-button' . $post_id_class . $class . $comment_class . '" data-nonce="' . $nonce . '" data-post-id="' . $post_id . '" data-iscomment="' . $is_comment . '" title="' . $title . '">' . $icon . $count . '</a>' . $loader . '</span>';
 	return $output;
 } // get_simple_likes_button()
 
